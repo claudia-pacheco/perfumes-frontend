74fe1a84e8bf980ee992648d433debac18c37cc9
Claudia <claudiaescola7@gmail.com>
2022-05-10 18:13:56 +0100

    frontend initial commit with fetched data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const [perfumes, setPerfumes] = useState([]);
+
+  useEffect(() => {
+    const fetchPerfumes = async () => {
+      const response = await fetch("http://localhost:8000/brands/");
+      const result = await response.json(response);
+      setPerfumes(result);
+    };
+    fetchPerfumes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        backgroundColor: "rgb(120, 52, 90)",
+      }}
+    >
+      <h1
+        style={{
+          fontSize: 44,
+          color: "yellow",
+          fontWeight: "bold",
+        }}
+      >
+        perfumes
+      </h1>
+      <div>
+        {perfumes.map((perfume) => (
+          <div
+            style={{
+              padding: 15,
+              fontSize: 24,
+              color: "yellow",
+              borderRadius: 5,
+              border: "2px solid yellow",
+            }}
+          >
+            {perfume.brand_name}
+            {<img className="dog-image" alt="Dog" src={perfume.logo} height='50' width='200' ></img>
+            }
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
