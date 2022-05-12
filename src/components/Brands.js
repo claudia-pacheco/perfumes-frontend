395007b8980bd823ba1e0387453c9c667267d4b2
Claudia <claudiaescola7@gmail.com>
2022-05-13 00:46:06 +0100

    brands page set up, styled and data is fetched

--- src/components/Brands.js
+++ src/components/Brands.js
@@ -1,32 +1,153 @@
 import { useEffect, useState } from "react";
-import theme from "./theme";
-import * as React from 'react';
+// import theme from "./theme";
+// import * as React from 'react';
+// import withRoot from './withRoot.js';
 
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Container from '@mui/material/Container';
-import Typography from './Typography';
-import useLocalStorage from "../hooks/useLocalStorage";
+// import Box from '@mui/material/Box';
+// import Grid from '@mui/material/Grid';
+// import Container from '@mui/material/Container';
+// import Typography from './Typography';
 import axios from "axios";
+import Button from './Button';
 
-const item = {
+// const item = {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center',
+//     px: 5,
+// };
+
+// function Brands() {
+//     const [brands, setBrands] = useState([]);
+
+//     useEffect(() => {
+//         axios({
+//             method: 'get',
+//             url: 'http://localhost:8000/brands/'
+//         })
+//             .then(response => {
+//                 console.log(`brands data: `);
+//                 console.log(response.data);
+
+//                 // reload
+//                 // Setting the data to State
+//                 setBrands(response.data);
+//             })
+//             .catch(error => {
+//                 console.log(error);
+//             })
+//     }, [])
+
+//     return (
+//         <Box component="section" sx={{ display: 'flex', overflow: 'hidden' }}>
+
+//             {brands ? brands.map(brand =>
+//                 <Container sx={{
+//                     mt: 3,
+//                     mb: 14,
+//                     display: 'flex',
+//                     flexDirection: 'column',
+//                     alignItems: 'center',
+//                 }}>
+//                     <Grid container spacing={5}>
+//                         <Grid item xs={12} md={6} >
+//                             <Box sx={item}>
+//                                 <Box
+//                                     component="img"
+//                                     src={brand.logo}
+//                                     alt="logo"
+//                                     sx={{ height: 55 }}
+//                                 />
+//                                 <Typography variant="h5" sx={{ my: 5 }}>
+//                                     {brand.description}
+//                                 </Typography>
+
+//                                 <Button color="secondary"
+//                                     variant="contained"
+//                                     size="large"
+//                                     component="a"
+//                                     href="/brands/{brand._id}"
+//                                 >More About {brand.brand_name}</Button></Box>
+//                         </Grid>
+//                     </Grid>
+//                 </Container>
+
+//             )
+
+
+//                 : <Container>Loading</Container>}
+//         </Box>
+//     );
+// }
+
+// export default withRoot(Brands)
+
+
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
+import Container from '@mui/material/Container';
+import Typography from '../components/Typography';
+import { Link } from "react-router-dom";
+
+const ImageBackdrop = styled('div')(({ theme }) => ({
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    background: '#000',
+    opacity: 0.5,
+    transition: theme.transitions.create('opacity'),
+}));
+
+const ImageIconButton = styled(ButtonBase)(({ theme }) => ({
+    position: 'relative',
     display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    px: 5,
-};
+    padding: 0,
+    borderRadius: 0,
+    height: '40vh',
+    [theme.breakpoints.down('md')]: {
+        width: '100% !important',
+        height: 100,
+    },
+    '&:hover': {
+        zIndex: 1,
+    },
+    '&:hover .imageBackdrop': {
+        opacity: 0.15,
+    },
+    '&:hover .imageMarked': {
+        opacity: 0,
+    },
+    '&:hover .imageTitle': {
+        border: '4px solid currentColor',
+    },
+    '& .imageTitle': {
+        position: 'relative',
+        padding: `${theme.spacing(2)} ${theme.spacing(4)} 14px`,
+    },
+    '& .imageMarked': {
+        height: 3,
+        width: 18,
+        background: theme.palette.common.white,
+        position: 'absolute',
+        bottom: -2,
+        left: 'calc(50% - 9px)',
+        transition: theme.transitions.create('opacity'),
+    },
+}));
+
+
 
 function Brands() {
     const [brands, setBrands] = useState([]);
-    const [accessToken] = useLocalStorage("accessToken", "");
 
     useEffect(() => {
         axios({
             method: 'get',
-            url: 'ttp://localhost:8000/brands/',
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            }
+            url: 'http://localhost:8000/brands/'
         })
             .then(response => {
                 console.log(`brands data: `);
@@ -39,50 +160,66 @@ function Brands() {
             .catch(error => {
                 console.log(error);
             })
+
     }, [])
 
-    return (
-        <Box>
 
-        </Box>
+    return (
+        <Container component="section" sx={{ mt: 8, mb: 4 }}>
+            <Typography variant="h4" marked="center" align="center" component="h2">
+                For all tastes and all desires
+            </Typography>
+            <Typography variant="body2" align="center" color="inherit" sx={{ mt: 2 }}>
+                Discover the experience
+            </Typography>
+            <Box sx={{ mt: 8, display: 'flex', flexWrap: 'wrap' }}>
+                {brands ? brands.map((brand) => (
+                    <ImageIconButton
+                        key={brand.id}
+                        component="a"
+                        href={`/brands/${brand.id}`}
+
+                    >
+                        <Box
+                            sx={{
+                                position: 'absolute',
+                                left: 0,
+                                right: 0,
+                                top: 0,
+                                bottom: 0,
+                                backgroundSize: 'cover',
+                                backgroundRepeat: 'no-repeat',
+                                backgroundPosition: 'center 40%',
+                                backgroundImage: `url(${brand.creators_pic})`,
+                            }}
+                        />
+                        <ImageBackdrop className="imageBackdrop" />
+                        <Button
+                            sx={{
+                                left: 0,
+                                right: 0,
+                                top: 0,
+                                bottom: 0,
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                color: 'common.white'
+                            }}
+                        >
+                            <Typography
+                                component="h3"
+                                variant="h6"
+                                color="inherit"
+                                className="imageTitle"
+                            >
+                                {brand.brand_name}
+                                <div className="imageMarked" />
+                            </Typography>
+                        </Button>
+                    </ImageIconButton>
+                )) : <Box> Loading</Box>}
+            </Box>
+        </Container >
     );
 }
-
-export default Brands;
-
-{/* <Box
-    component="section"
-    sx={{ display: 'flex', overflow: 'hidden' }}
->
-
-
-    <Container sx={{ mt: 15, mb: 30, display: 'flex', position: 'relative' }}>
-
-        <Box
-            component="img"
-            src=""
-            alt="Fragrance"
-            sx={{ pointerEvents: 'none', position: 'absolute', top: -180 }}
-        />
-        <Grid container spacing={5}>
-            <Grid item xs={12} md={4}>
-                <Box sx={item}>
-                    <Box
-                        component="img"
-                        src=""
-                        alt="Fragrance"
-                        sx={{ height: 55 }}
-                    />
-                    <Typography variant="h6" sx={{ my: 5 }}>
-                        Fragrance1
-                    </Typography>
-                    <Typography variant="h5">
-                        {'Fragrance caption'}
-                        {'caption continued.'}
-                    </Typography>
-                </Box>
-            </Grid>
-        </Grid>
-
-    </Container >
-</Box > */}
+export default Brands
