c21595e59c06f77f9373b1c6c33e11b375362d4f
Claudia <claudiaescola7@gmail.com>
2022-05-13 01:50:24 +0100

    formatted Nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,20 +2,20 @@ import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import Box from "@mui/material/Box";
 import { Button, Toolbar } from "@mui/material";
-import logo from "../assets/cloud9-logoo.png"
+import logo from "../assets/cloud9-logoo.png";
 function Nav() {
     return (
         <>
             <header>
-
-                <Toolbar justifyContent='space-between' position='fixed' >
-                    <img src={logo} height='100'></img>
+                <Toolbar justifyContent="space-between" position="fixed">
+                    <img src={logo} height="100"></img>
                     <Button href="/"> Home</Button>
                     <Button href="/brands"> Brands</Button>
                     <Button href="/perfumes">Perfumes</Button>
-                    <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+                    <Box sx={{ flex: 1, display: "flex", justifyContent: "flex-end" }}>
                         <Button href="/login">Sign In</Button>
-                        <Button href="/register">Register</Button></Box>
+                        <Button href="/register">Register</Button>
+                    </Box>
                 </Toolbar>
             </header>
             <main>
